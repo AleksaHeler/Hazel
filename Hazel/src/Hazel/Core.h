a4769b0acb40fc1ffed7848b98b43554d76e0655
aleksaheler <aleksaheler@gmail.com>
2021-02-24 11:26:25 +0100

    Update Core.h

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,6 +1,7 @@
 #pragma once
 // DLL import/export macro definitions for easier code reading
 
+// On windows set dll export/import macros
 #ifdef HZ_PLATFORM_WINDOWS
 	#ifdef HZ_BUILD_DLL
 		#define HAZEL_API __declspec(dllexport)
@@ -11,6 +12,12 @@
 	#error Hazel only supports Windows!
 #endif // HZ_PLATFORM_WINDOWS
 
+// If we are in debug mode, enable asserts
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif // HZ_DEBUG
+
+// Assert macros
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
