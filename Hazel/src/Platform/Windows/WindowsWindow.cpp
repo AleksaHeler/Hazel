db9563552656c59fb026a14b8a7fb58a2df64faf
aleksaheler <aleksaheler@gmail.com>
2021-02-25 21:22:31 +0100

    Added basic rendering context class

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -7,7 +7,7 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
-#include <glad/glad.h>
+#include  "Platform/OpenGL/OpenGLContext.h"
 
 namespace Hazel {
 	static bool s_GLFWInitialized = false;
@@ -54,11 +54,10 @@ namespace Hazel {
 
 		// Creating the glfw window, make context current, set a window user pointer
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-
-		// Initializing Glad
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+		
+		// Create and initialize context
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
 
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
@@ -161,7 +160,7 @@ namespace Hazel {
 	// OnUpdate
 	void WindowsWindow::OnUpdate() {
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	// SetVSync
--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Hazel/Window.h"
+#include "Hazel/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -30,7 +31,9 @@ namespace Hazel {
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
 
+	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData {
 			std::string Title;
