756c3e8eb4f12ba2c87664f06ffe5d25c86e16cd
aleksaheler <aleksaheler@gmail.com>
2021-02-27 17:49:45 +0100

    Renderer API abstraction

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -35,9 +35,6 @@ namespace Hazel {
 		// Vertex array
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
-		// Vertex buffer
-		glGenBuffers(1, &m_VertexBuffer);
-		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
 
 		// Three points with three dimensions
 		// Space is from -1 to 1
@@ -47,22 +44,16 @@ namespace Hazel {
 			 0.0f,  0.5f, 0.0f  // third point
 		};
 
-		// Send this data to GPU
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
 		// Tell OpenGL in what format the data is
 		glEnableVertexAttribArray(0);
 		// Describing data at index zero, three floats, dont normalize, space between points
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); 
 
-		// Index buffer
-		glGenBuffers(1, &m_IndexBuffer);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
-
 		// Tell in what order to connect vertices
-		unsigned int indices[3] = { 0, 1, 2 };
-		// Send this data to GPU
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		uint32_t indices[3] = { 0, 1, 2 };
+		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
 		// Vertext shader source code
 		// R"()" lets us have multiple line strings without needing to write " every line and so on
@@ -147,7 +138,7 @@ namespace Hazel {
 
 			// Draw the triangle we set up in Application constructor
 			glBindVertexArray(m_VertexArray);
-			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
 
 			// Call OnUpdate functions in all layers
 			for (Layer* layer : m_LayerStack)
